ecb8d6806ca2559bb59935cf4b28e9a17c54c414
Jomas0713 <jomascaguioa3@gmail.com>
2024-07-03 11:33:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-const btnLike1 = document.getElementById("btnLike1")
-const countLikes1 = document.getElementById("countLikes1")
-const btnLike2 = document.getElementById("btnLike2")
-const countLikes2 = document.getElementById("countLikes2")
+var product1 = document.getElementById("product1")
+            var qty1 = document.getElementById("qty1")
+            var price1 = document.getElementById("price1")
 
-function clickLike1(){
-    let totalLikes1 = parseInt(countLikes1.value) + 1
-    countLikes1.textContent = totalLikes1.toString()
-  }
-function clickLike2(){
-    let totalLikes2 = parseInt(countLikes2.value) + 1
-    countLikes2.textContent = totalLikes2.toString()
-}
-btnLike1.addEventListener("click",clickLike1)
-btnLike2.addEventListener("click",clickLike2)
+            var product2 = document.getElementById("product2")
+            var qty2 = document.getElementById("qty2")
+            var price2 = document.getElementById("price2")
 
-function DisLikes1() {
-  let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
-   countDisLikes1.textContent = totalDisLikes1.toString()
-}
-btnDisLike1.addEventListener("click",DisLikes1)
+            var carts = document.getElementById("carts")
+            var total = document.getElementById("total")
+            var cash = document.getElementById("cash")
+            var change = document.getElementById("change")
 
-function DisLikes2() {
-  let totalDisLikes2 = parseInt(countDisLikes2.value) + 1
-   countDisLikes2.textContent = totalDisLikes2.toString()
-}
-btnDisLike2.addEventListener("click",DisLikes2)
-
-const comment = document.getElementById("comment")
-const submit = document.getElementById("submit")
-const commentbox = document.getElementById("commentbox")
-
-function submitComment() {
-   commentbox.textContent += comment.value.toString() + "\n"
-    comment.value=""
-
-}
-submit.addEventListener("click",submitComment)
+            function addOrder() {
+                carts.textContent=""
+                if (parseFloat(qty1.value) > 0){
+                    var order = qty1.value.toString() + ' pc/s x ' + price1.textContent + '------'+ product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + '\n'
+                    //carts.textContent += carts.value.toString() + "\n";
+                    carts.textContent += order
+                }
+                if (parseFloat(qty2.value) > 0){
+                    var order = qty2.value.toString() + ' pc/s x ' + price2.textContent + '------' + product2.textContent + '------ Php' + (parseFloat(qty2.value)*parseFloat(price2.textContent)) + '\n'
+                    //carts.textContent += carts.value.toString() + "\n";
+                    carts.textContent += order
+            }
+            }
+            qty1.addEventListener("keyup", addOrder);
+            qty2.addEventListener("keyup", addOrder);
